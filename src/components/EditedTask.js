f7ab43e2d76d3cda8fd1af6baac2a11718efa336
David Zoufalý <davidzoufaly@gmail.com>
2019-06-09 10:38:39 +0200

    fixed uncontroled input error

--- src/components/EditedTask.js
+++ src/components/EditedTask.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class EditedTask extends React.Component {
-    state = {eItem: ""}
+  state = { eItem: this.props.item };
 
-    changeEdit = e => {
-        this.setState({
-          eItem: { name: e.target.value, key: this.state.eItem.key }
-        });
-      };
-    
-      submitEdit = () => {
-        this.props.submitEdit(this.state.eItem)
-        this.props.resetComponent()
-        this.setState({eItem: ""})
-    }
+  changeEdit = e => {
+    this.setState({
+      eItem: { name: e.target.value, key: this.state.eItem.key }
+    });
+  };
 
-    render() {
-        return (
-            <li className="task-item edit-item">
-            <input
-              value={this.state.eItem.name}
-              onChange={this.changeEdit}
-              autoFocus={true}
-            />
-            <button onClick={this.submitEdit}>Update</button>
-          </li>
-        )
-    }
-    componentDidMount() {
-        this.setState({eItem: this.props.item})
-    }
+  submitEdit = () => {
+    this.props.submitEdit(this.state.eItem);
+    this.props.resetComponent();
+    this.setState({ eItem: "" });
+  };
+
+  render() {
+    return (
+      <li className="task-item edit-item">
+        <input
+          value={this.state.eItem.name}
+          onChange={this.changeEdit}
+          autoFocus={true}
+        />
+        <button onClick={this.submitEdit}>Update</button>
+      </li>
+    );
+  }
 }
 
 export default EditedTask;
