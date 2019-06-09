763abbdd6a2764a071636373cc4c2bb7a4b42972
David Zoufalý <davidzoufaly@gmail.com>
2019-06-09 09:11:05 +0200

    removed uniqid dependency, key is generated by Date.now()

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,11 @@
 import React from "react";
-import uniqid from "uniqid";
 
 class Input extends React.Component {
   state = { inputedItem: { name: "", key: "", checked: false } };
 
   onInputChange = event => {
     this.setState({
-      inputedItem: { name: event.target.value, key: uniqid(), checked: false }
+      inputedItem: { name: event.target.value, key: Date.now(), checked: false }
     });
   };
 
